4d6bb01d5dd486697bd90ae2b2a4979c446e7662
Skriabin <artem.skriabin@distributedlab.com>
2021-01-28 14:21:13 +0200

    Move init to different container

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bandd init $1 --chain-id bandchain
+
+pwd
+
+cp ./$1/priv_validator_key.json ~/.bandd/config/priv_validator_key.json
+cp ./$1/node_key.json ~/.bandd/config/node_key.json
+cp ./genesis.json ~/.bandd/config/genesis.json
+
+

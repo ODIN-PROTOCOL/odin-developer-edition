35159d42eccb42905d0c71c3898c2c5e9d6426a8
StepanTita <stepan.tytarenko@nure.ua>
2021-02-25 16:56:49 +0200

    update data source and oracle scripts

---
+++ data-source-scripts/geo-data-v4.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import sys
+import requests
+
+BASE_DATA_SOURCES_URL = "http://35.195.150.105:5000/geo/data/devices"
+
+
+def main(models):
+    models = list(map(lambda x: x.lower(), models))
+    devices = requests.request("GET", BASE_DATA_SOURCES_URL).json()['message']
+    counts = [0] * len(models)
+    for device in devices:
+        model = device['model'].lower()
+        if model in models:
+            counts[models.index(model)] += 1
+    return ';'.join(list(map(str, counts)))
+
+
+if __name__ == "__main__":
+    try:
+        print(main([*sys.argv[1:]]), end='')
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        sys.exit(1)

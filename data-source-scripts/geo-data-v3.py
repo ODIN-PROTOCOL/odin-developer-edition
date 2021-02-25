813d2e98258da3d334057705c07d93d7675fd2fd
StepanTita <stepan.tytarenko@nure.ua>
2021-02-25 15:48:43 +0200

    change genesis

---
+++ data-source-scripts/geo-data-v3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+import requests
+
+BASE_DATA_SOURCES_URL = "http://35.195.150.105:5000/geo/data/devices"
+
+
+def main(uuids):
+    devices = requests.request("GET", BASE_DATA_SOURCES_URL).json()['message']
+    models = []
+    for device in devices:
+        if device['uuid'] in uuids:
+            models.append(device['model'])
+    return ';'.join(models)
+
+
+if __name__ == "__main__":
+    try:
+        print(main([*sys.argv[1:]]))
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        sys.exit(1)

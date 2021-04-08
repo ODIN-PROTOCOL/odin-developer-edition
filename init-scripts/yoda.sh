f2558f0ea20a187443dabac8c9d09c26c324e149
StepanTita <stepan.tytarenko@nure.ua>
2021-04-08 15:54:04 +0300

    moving to newer version of cosmos-sdk

--- init-scripts/yoda.sh
+++ init-scripts/yoda.sh
@@ -6,7 +6,7 @@ rm -rf ~/.yoda
 yoda config chain-id odin
 
 # add validator to yoda config
-yoda config validator $(bandcli keys show $1 -a --bech val --keyring-backend test)
+yoda config validator $(bandd keys show $1 -a --bech val --keyring-backend test)
 
 # setup execution endpoint
 yoda config executor "rest:https://iv3lgtv11a.execute-api.ap-southeast-1.amazonaws.com/live/master?timeout=10s"
@@ -20,18 +20,17 @@ yoda config rpc-poll-interval "1s"
 # setup max-try to yoda config
 yoda config max-try 5
 
-echo "y" | bandcli tx oracle activate --from odin1nnfeguq30x6nwxjhaypxymx3nulyspsuja4a2x --chain-id odin --broadcast-mode block --keyring-backend test
+echo "y" | bandd tx oracle activate --from odin1nnfeguq30x6nwxjhaypxymx3nulyspsuja4a2x --chain-id odin --broadcast-mode block --keyring-backend test --node $2
 
-for i in $(eval echo {1..$2})
-do
-  # add reporter key
-  yoda keys add reporter$i
-done
+yoda keys add reporter
 
 # send band tokens to reporters
-echo "y" | bandcli tx multi-send 1000000loki $(yoda keys list -a) --from $1 --broadcast-mode block --keyring-backend test
+for rec in $(yoda keys list -a)
+do
+  echo "y" | bandd tx bank send $1 $rec 1000000loki --broadcast-mode block --keyring-backend test --chain-id odin --node $2
+done
 
 # add reporter to bandchain
-echo "y" | bandcli tx oracle add-reporters $(yoda keys list -a) --from $1 --broadcast-mode block --keyring-backend test
+echo "y" | bandd tx oracle add-reporters $(yoda keys list -a) --from $1 --broadcast-mode block --keyring-backend test --chain-id odin --node $2
 
 echo "Yoda configured"

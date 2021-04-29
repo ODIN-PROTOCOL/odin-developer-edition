d38bcf7047802cecf678d7cf2f63221a417178c8
StepanTita <stepan.tytarenko@nure.ua>
2021-04-29 16:27:30 +0300

    add faucet

--- init-scripts/faucet.sh
+++ init-scripts/faucet.sh
@@ -3,12 +3,10 @@
 rm -rf ~/.faucet
 
 echo "usage ketchup faculty bench jewel rocket latin absurd decide field party reunion cook entry scout scene miss box memory museum decorate guide few verify" \
-    | bandd keys add $1 --recover --keyring-backend test
+    | faucet keys add $1 --recover
 
 faucet config chain-id odin
 
 faucet config port 5005
 
-faucet keys add worker
-
-echo "y" | bandd tx bank send $1 $(faucet keys show worker) 1000000000000loki --broadcast-mode block --keyring-backend test --node $2 --chain-id odin
+faucet config coins geo loki

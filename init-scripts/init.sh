bc8b8f72692092f958fdbe82ec7e171056d8a423
StepanTita <stepan.tytarenko@nure.ua>
2021-05-03 17:49:43 +0300

    enable unsafe cors

--- init-scripts/init.sh
+++ init-scripts/init.sh
@@ -7,5 +7,6 @@ cp ./$1/node_key.json ~/.band/config/node_key.json
 cp /genesis.json ~/.band/config/genesis.json
 
 sed -i 's/enable = false/enable = true/' ~/.band/config/app.toml
+sed -i 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/' ~/.band/config/app.toml
 
 cat ~/.band/config/app.toml

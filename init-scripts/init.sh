a8cfa62da4a66e85cfd756d746e3c1d75f5a6b20
Dima <slandyman9@gmail.com>
2021-11-29 14:23:17 +0200

    rename band to odin

--- init-scripts/init.sh
+++ init-scripts/init.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-bandd init $1 --chain-id odin --timeout-commit 5000
+odind init $1 --chain-id odin --timeout-commit 5000
 
-cp ./$1/priv_validator_key.json ~/.band/config/priv_validator_key.json
-cp ./$1/node_key.json ~/.band/config/node_key.json
-cp /genesis.json ~/.band/config/genesis.json
+cp ./$1/priv_validator_key.json ~/.odin/config/priv_validator_key.json
+cp ./$1/node_key.json ~/.odin/config/node_key.json
+cp /genesis.json ~/.odin/config/genesis.json
 
-sed -i 's/enable = false/enable = true/' ~/.band/config/app.toml
-sed -i 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/' ~/.band/config/app.toml
-sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = ["*"]/' ~/.band/config/config.toml
+sed -i 's/enable = false/enable = true/' ~/.odin/config/app.toml
+sed -i 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/' ~/.odin/config/app.toml
+sed -i 's/cors_allowed_origins = \[\]/cors_allowed_origins = ["*"]/' ~/.odin/config/config.toml
 
-#cat ~/.band/config/app.toml
+#cat ~/.odin/config/app.toml
--- init-scripts/yoda.sh
+++ init-scripts/yoda.sh
@@ -6,7 +6,7 @@ rm -rf ~/.yoda
 yoda config chain-id odin
 
 # add validator to yoda config
-yoda config validator $(bandd keys show $1 -a --bech val --keyring-backend test)
+yoda config validator $(odind keys show $1 -a --bech val --keyring-backend test)
 
 # setup execution endpoint
 yoda config executor "rest:https://iv3lgtv11a.execute-api.ap-southeast-1.amazonaws.com/live/master?timeout=10s"
@@ -20,17 +20,17 @@ yoda config rpc-poll-interval "1s"
 # setup max-try to yoda config
 yoda config max-try 5
 
-echo "y" | bandd tx oracle activate --from odin1nnfeguq30x6nwxjhaypxymx3nulyspsuja4a2x --chain-id odin --broadcast-mode block --keyring-backend test --node $2
+echo "y" | odind tx oracle activate --from odin1nnfeguq30x6nwxjhaypxymx3nulyspsuja4a2x --chain-id odin --broadcast-mode block --keyring-backend test --node $2
 
 yoda keys add reporter
 
-# send band tokens to reporters
+# send odin tokens to reporters
 for rec in $(yoda keys list -a)
 do
-  echo "y" | bandd tx bank send $1 $rec 1000000loki --broadcast-mode block --keyring-backend test --chain-id odin --node $2
+  echo "y" | odind tx bank send $1 $rec 1000000loki --broadcast-mode block --keyring-backend test --chain-id odin --node $2
 done
 
-# add reporter to bandchain
-echo "y" | bandd tx oracle add-reporters $(yoda keys list -a) --from $1 --broadcast-mode block --keyring-backend test --chain-id odin --node $2
+# add reporter to odinchain
+echo "y" | odind tx oracle add-reporters $(yoda keys list -a) --from $1 --broadcast-mode block --keyring-backend test --chain-id odin --node $2
 
 echo "Yoda configured"
